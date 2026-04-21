6af9f5f31440694dd81d37589024a67fa52ac324
조영래 <take.igjo@gmail.com>
2026-04-21 14:25:06 +0900

    [FEAT] CourseRepository 인터페이스 생성, 기존 CourseRepository-> InMemoryCourseRepository로 리팩토링

--- src/main/java/com/project/lxp/Course.java
+++ src/main/java/com/project/lxp/Course.java
@@ -15,6 +15,75 @@ public class Course {
     public LocalDateTime update_at;
     public LocalDateTime published_at;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getInstructorId() {
+        return instructorId;
+    }
+
+    public void setInstructorId(Long instructorId) {
+        this.instructorId = instructorId;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+
+    public LocalDateTime getCreate_at() {
+        return create_at;
+    }
+
+    public void setCreate_at(LocalDateTime create_at) {
+        this.create_at = create_at;
+    }
+
+    public LocalDateTime getUpdate_at() {
+        return update_at;
+    }
+
+    public void setUpdate_at(LocalDateTime update_at) {
+        this.update_at = update_at;
+    }
+
+    public LocalDateTime getPublished_at() {
+        return published_at;
+    }
+
+    public void setPublished_at(LocalDateTime published_at) {
+        this.published_at = published_at;
+    }
 }
